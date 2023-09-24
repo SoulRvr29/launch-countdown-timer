e4e1f64bd20b6aacb879025e2a43435dc6809665
Paweł Chudecki <pawelc29@interia.pl>
2023-09-24 15:04:18 +0200

    Revert "anim update"
    This reverts commit 8a88934c032506e191695d10e54f12d88d72d39d.

--- src/TimerBlock.jsx
+++ src/TimerBlock.jsx
@@ -19,25 +19,18 @@ const TimerBlock = ({ type, value }) => {
   return (
     <div>
       <div
-        id="number-block"
-        className="number-block relative w-[4.5rem] h-16 drop-shadow-lg grid place-content-center"
+        id="block"
+        className="relative w-[4.5rem] h-16 drop-shadow-lg grid place-content-center overflow-hidden bg-Dark-desaturated-blue rounded-md"
       >
+        <div className="">{value}</div>
         <div
           id="upper-half"
-          className="absolute  brightness-90 w-full h-16 rounded-md overflow-hidden"
-        >
-          <div className="bg-Dark-desaturated-blue w-full h-8 overflow-hidden absolute">
-            <div className="absolute w-full top-3">{value}</div>
-          </div>
-        </div>
-        <div
-          id="lower-half"
-          className="absolute bottom-0  brightness-110 w-full h-16 rounded-md overflow-hidden"
-        >
-          <div className="bg-Dark-desaturated-blue w-full h-8 overflow-hidden absolute bottom-0">
-            <div className="absolute w-full bottom-3">{test}</div>
-          </div>
-        </div>
+          className="absolute bg-Dark-blue w-full h-8 rounded-t-md opacity-[30%]"
+        ></div>
+        {/* <div
+                  id="lower-half"
+                  className="absolute bottom-0 bg-Dark-desaturated-blue w-full h-8 rounded-b-md"
+                ></div> */}
         <div
           id="left-dot"
           className="h-[6px] w-[6px] bg-Dark-blue absolute rounded-full top-[calc(50%-3px)] -left-[3px] z-10"
@@ -47,10 +40,7 @@ const TimerBlock = ({ type, value }) => {
           className="h-[6px] w-[6px] bg-Dark-blue absolute rounded-full top-[calc(50%-3px)] -right-[3px] z-10"
         ></div>
       </div>
-      <div
-        id="block-type"
-        className="text-[0.45rem] text-Grayish-blue tracking-[3px] -mt-1 uppercase "
-      >
+      <div className="text-[0.45rem] text-Grayish-blue tracking-[3px] -mt-1 uppercase">
         {type}
       </div>
     </div>
