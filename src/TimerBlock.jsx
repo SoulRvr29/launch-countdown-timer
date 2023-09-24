ed3bace6442f664b993d5ce8e235284eda25c07b
Paweł Chudecki <pawelc29@interia.pl>
2023-09-24 15:50:38 +0200

    update new1

--- src/TimerBlock.jsx
+++ src/TimerBlock.jsx
@@ -1,36 +1,15 @@
-import { useState } from "react";
-
 const TimerBlock = ({ type, value }) => {
-  const [test, setTest] = useState(0);
-  setTimeout(() => {
-    document.querySelector(".number-block")((item) => {
-      if (type == "seconds") item.setAttribute("upper-value", value);
-    });
-  }, 500);
-  setTimeout(() => {
-    document.querySelector(".number-block")((item) => {
-      if (type == "seconds") item.setAttribute("lower-value", value - 1);
-    });
-    setTimeout(() => {
-      setTest(value - 1);
-    }, 750);
-  }, 1000);
-
   return (
     <div>
       <div
         id="block"
-        className="relative w-[4.5rem] h-16 drop-shadow-lg grid place-content-center overflow-hidden bg-Dark-desaturated-blue rounded-md"
+        className="relative w-[4.5rem] h-16 drop-shadow-lg grid place-content-center overflow-hidden bg-Dark-desaturated-blue rounded-md brightness-110"
       >
         <div className="">{value}</div>
         <div
           id="upper-half"
           className="absolute bg-Dark-blue w-full h-8 rounded-t-md opacity-[30%]"
         ></div>
-        {/* <div
-                  id="lower-half"
-                  className="absolute bottom-0 bg-Dark-desaturated-blue w-full h-8 rounded-b-md"
-                ></div> */}
         <div
           id="left-dot"
           className="h-[6px] w-[6px] bg-Dark-blue absolute rounded-full top-[calc(50%-3px)] -left-[3px] z-10"
@@ -40,7 +19,7 @@ const TimerBlock = ({ type, value }) => {
           className="h-[6px] w-[6px] bg-Dark-blue absolute rounded-full top-[calc(50%-3px)] -right-[3px] z-10"
         ></div>
       </div>
-      <div className="text-[0.45rem] text-Grayish-blue tracking-[3px] -mt-1 uppercase">
+      <div className="scale-[60%] text-Grayish-blue text-[3px] tracking-[5px]  uppercase w-[4.5rem] text">
         {type}
       </div>
     </div>
