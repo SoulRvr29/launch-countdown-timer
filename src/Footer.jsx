8706f39a9f09ff8ec20e4cbb42efbe530385feae
Paweł Chudecki <pawelc29@interia.pl>
2023-09-25 10:43:53 +0200

    complete layout

---
+++ src/Footer.jsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+  return (
+    <footer className="text-base text-center text-Grayish-blue p-4 max-sm:text-xs absolute bottom-0 mx-auto w-screen z-10">
+      Challenge by
+      <a
+        className="text-Soft-red ml-2 hover:text-White hover:underline transition-all duration-300"
+        href="https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by
+      <a
+        className="text-Soft-red ml-2 hover:text-White hover:underline transition-all duration-300"
+        href="https://www.frontendmentor.io/profile/SoulRvr29"
+      >
+        Paweł Chudecki
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;
--- src/TimerBlock.jsx
+++ src/TimerBlock.jsx
@@ -3,23 +3,23 @@ const TimerBlock = ({ type, value }) => {
     <div>
       <div
         id="block"
-        className="relative w-[4.5rem] h-16 drop-shadow-lg grid place-content-center overflow-hidden bg-Dark-desaturated-blue rounded-md brightness-110"
+        className="relative max-md:w-[4.5rem] w-[9.375rem] max-md:h-16 h-[140px]  max-md:drop-shadow-[0_4px_0_rgba(0,0,0,0.2)] drop-shadow-[0_10px_0_rgba(0,0,0,0.2)] grid place-content-center overflow-hidden bg-Dark-desaturated-blue max-md:rounded-md rounded-lg brightness-110 z-20"
       >
-        <div className="">{value}</div>
+        <div className="max-md:text-4xl text-[5rem]">{value}</div>
         <div
           id="upper-half"
-          className="absolute bg-Dark-blue w-full h-8 rounded-t-md opacity-[30%]"
+          className="absolute bg-[#100d1f] w-full max-md:h-8 h-[70px] rounded-t-md opacity-[30%] border-b border-black"
         ></div>
         <div
           id="left-dot"
-          className="h-[6px] w-[6px] bg-Dark-blue absolute rounded-full top-[calc(50%-3px)] -left-[3px] z-10"
+          className="max-md:h-[6px] max-md:w-[6px] w-3 h-3 bg-Dark-blue absolute rounded-full max-md:top-[calc(50%-3px)] max-md-left-[3px]  top-[calc(50%-6px)] -left-[6px] z-10"
         ></div>
         <div
           id="right-dot"
-          className="h-[6px] w-[6px] bg-Dark-blue absolute rounded-full top-[calc(50%-3px)] -right-[3px] z-10"
+          className="max-md:h-[6px] max-md:w-[6px] w-3 h-3 bg-Dark-blue absolute rounded-full max-md:top-[calc(50%-3px)] max-md-right-[3px] top-[calc(50%-6px)] -right-[6px] z-10"
         ></div>
       </div>
-      <div className="scale-[60%] text-Grayish-blue text-[3px] tracking-[5px]  uppercase w-[4.5rem] text">
+      <div className="max-md:scale-[60%] text-Grayish-blue max-md:text-[3px] text-sm tracking-[5px]  uppercase max-md:w-[4.5rem] max-md:mt-2 mt-6 ">
         {type}
       </div>
     </div>
